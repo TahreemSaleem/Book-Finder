dcd21c22ad24ecf89ae57c6cd066304e3513de77
TahreemSaleem <tahreem.saleem9@gmail.com>
2019-06-17 15:22:42 +0500

    pagination added

--- js/index.js
+++ js/index.js
@@ -1,29 +1,91 @@
 //const url = 'https://www.goodreads.com/search.xml?key=o3ZEitH9EZ1E7JYpQ6BHHQ&q=Ender%27s+Game&search=title';
 const search_url = new URL('https://www.goodreads.com/search.xml')
 const key = 'o3ZEitH9EZ1E7JYpQ6BHHQ'
-
+let titles;
+let page_index;
+const items_per_page = 5;
+let total_items;
+let total_pages;
 
 function getTitle(xmlDoc) {
     console.log(xmlDoc);
-    let titles = xmlDoc.getElementsByTagName("title");
-    let ul = document.getElementById("book-list")
+    let titles_xml = xmlDoc.getElementsByTagName("title");
+    localStorage.setItem('xml', new XMLSerializer().serializeToString(xmlDoc));
+    titles = [];
+    page_index = 0;
+    for (title of titles_xml) {
+        titles.push(title.innerHTML);
+    }
+    total_items = titles.length;
+    total_pages = total_items/items_per_page;
+    titles = splitList(titles);
+    displayList();
+    document.getElementById("left-arrow").style.visibility = 'hidden';
+    document.getElementById("right-arrow").style.visibility = 'visible';
+}
 
-    for (title of titles) {
+function displayList() {
+    
+    let ul = document.getElementById("book-list");
+    ul.innerHTML = "";
+    for (title of titles[page_index]) {
+        // console.log(title)
         let li = document.createElement('li');
         let a = document.createElement('a');
-        let linkText = document.createTextNode(title.innerHTML);
+        let linkText = document.createTextNode(title);
         a.appendChild(linkText);
-        a.onclick = function(){ 
-            // getBookDetail(this.text,xmlDoc)
-            localStorage.setItem('xml',new XMLSerializer().serializeToString(xmlDoc));
-        };
-        a.href = 'detail_page.html'; 
+        // a.onclick = function() {
+        //     // getBookDetail(this.text,xmlDoc)
+        //     localStorage.setItem('xml', new XMLSerializer().serializeToString(xmlDoc));
+        // };
+        a.href = 'detail_page.html';
         li.appendChild(a);
         ul.appendChild(li);
     }
 }
 
-function getBookDetail(){
+function click(){
+    // getBookDetail(this.text,xmlDoc)
+    localStorage.setItem('xml', new XMLSerializer().serializeToString(xmlDoc));
+}
+function next(){
+    
+    if (page_index < total_pages-1){
+        page_index ++
+        displayList();
+    }
+    else {
+        document.getElementById("right-arrow").style.visibility = 'hidden';   
+    }
+    if (document.getElementById("left-arrow").style.visibility == 'hidden'){
+        document.getElementById("left-arrow").style.visibility = 'visible';
+    }
+
+}
+function previous(){
+    if (page_index != 0){
+        page_index --
+        displayList();
+    }
+    else{
+        document.getElementById("left-arrow").style.visibility = 'hidden';  
+    }
+    if (document.getElementById("right-arrow").style.visibility == 'hidden'){
+        document.getElementById("right-arrow").style.visibility = 'visible';
+    }
+}
+
+// function showArrows() {
+//     if(disp == 'none') document.getElementsByClassName("arrow").style.display = 'none';
+//    }
+function splitList(list) {
+    let splited = []
+    for (var i = 0; i < list.length; i += items_per_page)
+        splited.push(list.slice(i, i + items_per_page));
+    return splited;
+}
+
+function getBookDetail() {
     const xmlDoc = $.parseXML(localStorage.getItem('xml'));
     let title = xmlDoc.getElementsByTagName("title")[0].innerHTML;
     let author = xmlDoc.getElementsByTagName("name")[0].innerHTML;
@@ -52,7 +114,7 @@ function handleResponse(response) {
     return response.text()
         .then(xml => {
             if (response.ok) {
-                
+
                 return Promise.resolve($.parseXML(xml));
             } else {
                 return Promise.reject(xml);
@@ -62,5 +124,5 @@ function handleResponse(response) {
 
 function getBooks() {
     const book = document.getElementById('book-name').value;
-    getData(search_url, book,getTitle);
+    getData(search_url, book, getTitle);
 }
